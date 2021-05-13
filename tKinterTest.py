e684ad95505e2ca008f1e73636e7b04f9a4bbc2c
conservativeghosts <77815037+conservativeghosts@users.noreply.github.com>
2021-05-12 19:02:53 -0600

    Add files via upload

--- tKinterTest.py
+++ tKinterTest.py
@@ -1,30 +1,87 @@
 from tkinter import *
 
 top = Tk()
-groceryList = []
+playList = []
 
 def results():
-    print(groceryList)
+    print(playList)
 
-def add():
-    newItem = E1.get()
-    groceryList.append(newItem)
+def export():
+    with open("test.txt", "w") as f:
+        for item in playList:
+            f.write("%s\n" % item)
+def clearWindow():
+    for widgets in top.winfo_children():
+        widgets.destroy()
 
-#This is a label widget
-L1 = Label(top, text = "Grocery List")
-L1.grid(column= 0, row= 1)
+def mainMenu():
+    clearWindow()
+    LMain = Label(top, text = "Block 5 GUI Projects")
+    LMain.grid(column = 2, row = 1)
 
-#This is an entry widget
-E1 = Entry(top, bd = 5)
-E1.grid(column = 0, row = 2)
+    B1Main = Button(text = "Week 1", bg = "white", command = week1)
+    B1Main.grid(column = 2, row = 2)
 
-#button widget
-B2 = Button(text = "Add", bg = "cyan", command = add)
-B2.grid(column = 1, row = 2)
+    B2Main = Button(text = "Week 2", bg = "white", command = week2)
+    B2Main.grid(column = 2, row = 3)
 
-#button widget
-B1 = Button(text = "   Print List   ", bg = "#fce0de", command = results)
-B1.grid(column = 0, row = 3)
+    B3Main = Button(text = "Week 3", bg = "white", command = week3)
+    B3Main.grid(column = 2, row = 4)
 
+def week1():
+    def add():
+        newItem = E1.get()
+        playList.append(newItem)
+        E1.delete(0, END)
 
-top.mainloop()
+    clearWindow()
+
+    #This is a label widget
+    L1 = Label(top, text = "Playlist Maker")
+    L1.grid(column = 0, row = 1)
+
+    #This is an entry widget
+    E1 = Entry(top, bd = 5)
+    E1.grid(column = 0, row = 2)
+
+    #button widgets
+    B1 = Button(text = "   Print Playlist   ", bg = "#c6edee", command = results)
+    B1.grid(column = 0, row = 3)
+
+    B2 = Button(text = "Add", bg = "#feebe5", command = add)
+    B2.grid(column = 1, row = 2)
+
+    B3 = Button(text = "  export list  ", bg = "#ceb0af" ,command = export)
+    B3.grid(column = 0, row = 5)
+
+    Bclear = Button(text = "Main Menu", bg = "white", command = mainMenu)
+    Bclear.grid(column = 3, row = 1)
+
+def week2():
+    clearWindow()
+    
+    L1W2 = Label(top, text = "Dice Rollaer Program")
+    L1W2.grid(column = 0, row = 1)
+    
+    L2W2 = Label(top, text = "How many sides")
+    L2W2.grid(column = 0, row = 2)
+
+    L3W2= Label(top, text = "How many rolls")
+    L3W2.grid(column = 2, row = 2)
+
+    E1W2 = Entry(top, bd = 5)
+    E1W2.grid(column = 0, row = 3)
+    
+    E2W2 = Entry(top, bd = 5)
+    E2W2.grid(column = 2, row = 3)
+    
+    B1W2 = Button(text = "roll", bg = "yellow")
+    B1W2.grid(column = 2, row = 4)
+#to add: roll funciton and exit button
+
+def week3():
+    print("we're not here yet")
+
+if __name__ == "__main__":
+    mainMenu()
+    top.mainloop()
